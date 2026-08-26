729ca7e9bd2cae96d27879665573c546dbb02367
zxcwave <90509123+julverik@users.noreply.github.com>
2026-08-26 18:11:10 +0300

    add ouput.css for tailwind

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Устанавливаем Node.js для Render
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
 
-# Устанавливаем зависимости
 make install
+
 npm install
 
-# Собираем стили через Django команду
+ls -la task_manager/static/css/input.css
+
 uv run python manage.py tailwind build
 
-# Собираем статику и миграции
+ls -la task_manager/static/css/output.css
+
 make collectstatic
+
 make migrate
