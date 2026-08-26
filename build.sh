23f3d2bc9d8d2eb3baa9aad297cd8a827a915256
zxcwave <90509123+julverik@users.noreply.github.com>
2026-08-26 18:01:28 +0300

    Add talwind through django

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
+# Устанавливаем Node.js для Render
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
 
+# Устанавливаем зависимости
 make install
 npm install
 
-./node_modules/.bin/tailwindcss -i task_manager/static/css/input.css -o task_manager/static/css/output.css
+# Собираем стили через Django команду
+uv run python manage.py tailwind build
 
-make collectstatic && make migrate
+# Собираем статику и миграции
+make collectstatic
+make migrate
