0967e0a4a35ee3068591e56e578167e58e239e07
zxcwave <90509123+julverik@users.noreply.github.com>
2026-09-08 01:17:59 +0300

    fix urls

--- src/hexlet_code/urls.py
+++ src/hexlet_code/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import include, path
 
 from task_manager import views
+from users.views import CustomLoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("users/", include("users.urls")),
     path("statuses/", include("statuses.urls")),
-    path("tasks/", include("tasks.urls")),
     path("labels/", include("labels.urls")),
+    path("tasks/", include("tasks.urls")),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
